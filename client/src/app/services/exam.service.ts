fee13be923f86c81e620dc63c1b1743ed270ac59
cpaolo74@hotmail.it <cpaolo74@hotmail.it>
2023-05-01 23:14:46 +0000

    File generated by Skaffolder - user: cpaolo74@hotmail.it

---
+++ client/src/app/services/exam.service.ts
@@ -0,0 +1,22 @@
+// BASE SERVICE
+import { ExamBaseService } from './base/exam.base.service';
+
+
+// start documentation
+/**
+ * Custom APIs
+ *
+ * Service.validate
+ *	@description questa Api serve per validare gli esami
+ *	@param String id id esami
+ *	@returns Boolean
+ *
+ */
+// end documentation
+
+/**
+ * YOU CAN OVERRIDE HERE examBaseService
+ */
+export class ExamService extends ExamBaseService {
+
+}
